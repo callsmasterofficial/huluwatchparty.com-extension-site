55b6b21e73e2675329a010b0c1f5209060ddac2c
yash <yashvermacallsmaster@gmail.com>
2023-04-29 18:15:02 +0530

    fds

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,17 +1,39 @@
-import db from "../../config/db";
-import Blog from "../../models/blog"
-import siteConfig from "../../siteConfig";
+import db from '../../config/db'
+import Blog from '../../models/blog'
+import siteConfig from '../../siteConfig'
 
+export default async function handler(req, res) {
+  try {
+    await db()
+    const blogs = await Blog.find({ site: siteConfig.siteId }, '-_id slug')
+    res.setHeader('Content-Type', 'text/xml')
 
-export default async function handler(
-  req,
-  res
-) {
-  try{
-    await db();
-    const blogs=await Blog.find({site:siteConfig.siteId},'-_id slug');
-    res.send(blogs)
-  }catch(err){
-    res.status(500).json({message:err.message});
+    // Instructing the Vercel edge to cache the file
+    res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
+
+    // generate sitemap here
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
+      <url>
+            <loc>${siteConfig.url}</loc>
+        </url>
+        <url>
+            <loc>${siteConfig.url}blog/</loc>
+        </url>
+      ${blogs
+        .map(({ slug }) => {
+          return `
+                <url>
+                    <loc>${`${siteConfig.url}blog/${encodeURIComponent(
+                      slug
+                    )}`}</loc>
+                </url>
+            `
+        })
+        .join('')}
+      </urlset>`
+    res.end(xml)
+  } catch (err) {
+    res.send({ err })
   }
 }
